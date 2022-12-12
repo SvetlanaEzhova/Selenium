95f43568bb081032278bb18467225055276d8a3d
Svetlana Ezhova <sezhova@ptsecurity.com>
2022-12-12 20:25:37 +0300

    Задание 11 к Занятию 6

---
+++ Lesson06/task6_11.py
@@ -0,0 +1,141 @@
+''' Сделайте сценарий для регистрации нового пользователя в учебном приложении litecart (не в админке, а в клиентской части магазина).
+
+Сценарий должен состоять из следующих частей:
+
+1) регистрация новой учётной записи с достаточно уникальным адресом электронной почты (чтобы не конфликтовало с 
+ранее созданными пользователями, в том числе при предыдущих запусках того же самого сценария),
+2) выход (logout), потому что после успешной регистрации автоматически происходит вход,
+3) повторный вход в только что созданную учётную запись,
+4) и ещё раз выход.
+
+В качестве страны выбирайте United States, штат произвольный. При этом формат индекса -- пять цифр.
+
+Можно оформить сценарий либо как тест, либо как отдельный исполняемый файл.
+
+Проверки можно никакие не делать, только действия -- заполнение полей, нажатия на кнопки и ссылки. 
+Если сценарий дошёл до конца, то есть созданный пользователь смог выполнить вход и выход -- значит создание прошло успешно.
+
+В форме регистрации есть капча, её нужно отключить в админке учебного приложения на вкладке Settings -> Security. '''
+
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+
+class User:
+    ''' структура со всеми полями и значениями по умолчанию для пользователя'''
+
+    def __init__(
+        self,
+        firstname: str = 'John',
+        lastname: str = 'Doe',
+        address: str = 'somewhere',
+        postcode: str = '12345',
+        city: str = 'San Jose',
+        country: str = 'United States',
+        zone: str = 'California',
+        phone: str = '+11231234567',
+        email: str = 'user@domain.com',
+        password: str = 'password'
+    ) -> None:
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.postcode = postcode
+        self.city = city
+        self.country = country
+        self.zone = zone
+        self.phone = phone
+        self.email = email
+        self.password = password
+
+
+class driver_litecart(webdriver.Chrome):
+    ''' класс с create\logout\login'''
+
+    def create_new_user(self, user: User):
+        ''' Нажимаем "Создать нового пользователя", заполняем все требуемые поля и нажимаем "Создать" '''
+
+        self.find_element(By.CSS_SELECTOR, 'div#box-account-login a[href*=create_account]').click()
+        WebDriverWait(driver=self, timeout=5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name=create_account]'))
+        )
+
+        self.find_element(By.CSS_SELECTOR, 'input[name=firstname]').send_keys(user.firstname)
+        self.find_element(By.CSS_SELECTOR, 'input[name=lastname]').send_keys(user.lastname)
+        self.find_element(By.CSS_SELECTOR, 'input[name=address1]').send_keys(user.address)
+        self.find_element(By.CSS_SELECTOR, 'input[name=postcode]').send_keys(user.postcode)
+        self.find_element(By.CSS_SELECTOR, 'input[name=city]').send_keys(user.city)
+
+        self.find_element(By.CSS_SELECTOR, 'span.selection').click()
+        self.find_element(By.CSS_SELECTOR,
+                          'input.select2-search__field').send_keys(user.country + Keys.ENTER)
+
+        self.find_element(By.CSS_SELECTOR,
+                          'select[name=zone_code]').send_keys(user.zone + Keys.ENTER)
+
+        self.find_element(By.CSS_SELECTOR, 'input[name=email]').send_keys(user.email)
+        self.find_element(By.CSS_SELECTOR, 'input[name=phone]').send_keys(user.phone)
+
+        self.find_element(By.CSS_SELECTOR, 'input[name=password]').send_keys(user.password)
+        self.find_element(By.CSS_SELECTOR,
+                          'input[name=confirmed_password]').send_keys(user.password)
+
+        self.find_element(By.CSS_SELECTOR, 'button[name=create_account]').click()
+        WebDriverWait(driver=self, timeout=5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div#box-account'))
+        )
+
+    def login(self, user: User):
+        ''' Нажимаем "Домой", заполняем email\пароль и нажимаем "Логин" '''
+
+        self.find_element(By.CSS_SELECTOR, 'div#logotype-wrapper').click()
+        WebDriverWait(driver=self, timeout=5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div#box-account-login'))
+        )
+
+        self.find_element(By.CSS_SELECTOR, 'input[name=email]').send_keys(user.email)
+        self.find_element(By.CSS_SELECTOR, 'input[name=password]').send_keys(user.password)
+
+        self.find_element(By.CSS_SELECTOR, 'button[name=login]').click()
+        WebDriverWait(driver=self, timeout=5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div#box-account'))
+        )
+
+    def logout(self):
+        ''' Нажимаес "Logout" '''
+        self.find_element(By.CSS_SELECTOR, 'div#box-account a[href*=logout]').click()
+        WebDriverWait(driver=self, timeout=15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div#box-account-login'))
+        )
+
+
+if __name__ == "__main__":
+
+    driver = driver_litecart()
+    driver.get(url="http://localhost/litecart/")
+
+    # пользователь с с достаточно уникальным email (dd<ДатаЧасыМинутыСекунды>@mail.com)
+    user = User(email='dd' + datetime.now().strftime('%d%H%M%S') + '@mail.com')
+    result = True
+
+    try:
+        driver.create_new_user(user)
+        driver.logout()
+        driver.login(user)
+        driver.logout()
+
+    except Exception as ex:
+        result = False
+        print(f"Exception: {type(ex).__name__}")
+        print(ex)
+        # если упали, то делаем скриншот, сохраняется в текущей папке
+        driver.save_screenshot('scr_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.png')
+
+    finally:
+        print('\nРезультат теста:', 'Пройден успешно' if result else '!!! Ошибка !!!', '\n')
+        driver.quit()
